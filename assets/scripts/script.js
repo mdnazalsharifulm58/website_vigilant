73c18178d9267fca6c2c6a8dd500980387e05b38
mdnazalsharifulm58 <mdnazalsharifulm2@gmail.com>
2020-01-08 10:58:23 -0800

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+  $('.slick.marquee').slick({
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    centerMode: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    arrows: false,
+    buttons: false
+  });
+});
